49d04ec4c984b535f752e96328d9e8c24ed3f7b9
GiannisGial <129963579+Giannislar@users.noreply.github.com>
2025-07-01 14:17:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define a cache name
-const CACHE_NAME = 'peiniata-calculator-v1';
+const CACHE_NAME = 'peiniata-calculator-v2';
 
 // List of files to cache. Make sure these paths match your files on GitHub.
 const urlsToCache = [
